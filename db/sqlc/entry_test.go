e95688429efb216803483ddf5bdab8202f637802
valkyraycho <razor0424@gmail.com>
2025-01-17 15:04:34 -0800

    add entry tests

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/valkyraycho/bank/utils"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	args := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    utils.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, args.AccountID, entry.AccountID)
+	require.Equal(t, args.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t, createRandomAccount(t))
+}
+
+func TestGetEntry(t *testing.T) {
+	entry := createRandomEntry(t, createRandomAccount(t))
+	resEntry, err := testQueries.GetEntry(context.Background(), entry.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, resEntry)
+
+	require.Equal(t, entry.ID, resEntry.ID)
+	require.Equal(t, entry.AccountID, resEntry.AccountID)
+	require.Equal(t, entry.Amount, resEntry.Amount)
+	require.Equal(t, entry.CreatedAt, resEntry.CreatedAt)
+}
+
+func TestListEntrys(t *testing.T) {
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+	args := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, args.AccountID, entry.AccountID)
+	}
+
+}
