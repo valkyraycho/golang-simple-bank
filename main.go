35da1a27e0c483609615395a5546766d07573093
valkyraycho <razor0424@gmail.com>
2025-01-19 16:32:55 -0800

    fix main

--- main.go
+++ main.go
@@ -21,7 +21,11 @@ func main() {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
-	server := api.NewServer(db.NewStore(conn))
+	server, err := api.NewServer(cfg, db.NewStore(conn))
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
 	err = server.Start(cfg.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start the server: ", err)
